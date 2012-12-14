23ac931d686d2a88821aa34d49bfec048a450032
Timothy Slatcher <timothy.slatcher@magd.ox.ac.uk>
2012-12-14 16:38:48 +0000

    slight optimization, next step is to rewrite check to be faster

--- narcissistic.hs
+++ narcissistic.hs
@@ -1,16 +1,23 @@
+import Data.List
+import Data.Maybe
+
 -- A solution for http://programmingpraxis.com/2012/12/14/115132219018763992565095597973971522401/
 
--- Check the n-digit number to see if it is narcissistic. Numbers stored LSB first. So "check 3 351 == True"
-check n xs		= if (expand xs) == sum (map (\x -> x^n) xs) then True else False
+-- Check the n-digit number to see if it is narcissistic for any permuation of the n bits. So "check 3 351 == True"
+check n xs p	= foldl (\ys x -> if (expand x) == sum (map (\x -> p!!x) xs) then Just x else ys) Nothing (filter (\x -> (last x) /= 0) (permutations xs))
 -- Converts LSB first numbers into Integers
 expand []		= 0
 expand (x:xs) 	= x+(10* (expand xs))
 
--- Lists all numbers of length n in LSB first reperesentation
-allNumbers 0	= [[0]]
-allNumbers 1	= map (\x -> [x]) [1..9]
-allNumbers n 	= concat (map (\x -> map (\y -> y:x) [0..9]) (allNumbers (n-1)))
+-- Lists all nondecreasing numbers
+allNumbers 0	= [(0,[0])]
+allNumbers 1	= map (\x -> (x, [x])) [0..9]
+allNumbers n 	= concat (map (\(z,x) -> map (\y -> (y,(y:x))) [z..9]) (allNumbers (n-1)))
+allNs n			= map snd (allNumbers n)
+
+-- We can speed up checking by caching [0..9] to the power of n
+powers n 		= map (^n) [0..9]
 -- List narcissistic numbers of length n
-narcissistic n 	= filter (check n) (allNumbers n)
+narcissistic n 	= catMaybes (map (\xs ->check n xs (powers n)) (allNs n))
 
 allNarcs 		= map expand (concat (map narcissistic [0..]))
