75a25cebb6e296539444475f0de434c11a8cbf19
Timothy Slatcher <timothy.slatcher@magd.ox.ac.uk>
2012-12-14 16:38:25 +0000

    whitespace fix

--- sequenceGen.hs
+++ sequenceGen.hs
@@ -6,7 +6,7 @@ split xs n	= (take n xs, take n (drop n xs))
 check xs	= foldl (\a (ys, zs) -> if ys == zs then True else a) False (map (split xs) [1..((length xs) `div` 2)])
 
 -- The alphabet from which we draw the characters
-alphabet = [1,2,3]
+alphabet 	= [1,2,3]
 -- Generate all sequences of length n
-generate 0 = [[]]
-generate n = foldl (\ys zs -> if check zs then ys else zs:ys) [] (concat ( map (\x -> map (\y -> y:x) alphabet) (generate (n-1))))
+generate 0 	= [[]]
+generate n 	= foldl (\ys zs -> if check zs then ys else zs:ys) [] (concat ( map (\x -> map (\y -> y:x) alphabet) (generate (n-1))))
